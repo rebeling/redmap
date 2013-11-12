ad24262817a052067a23121676c4ce801b36bd21
rebel <rebel@retresco.de>
2013-11-12 16:06:50 +0100

    log request status and url

--- redmine/redreq.py
+++ redmine/redreq.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # encoding: utf-8
 import requests
-# import logging as log
+import logging as log
 
 
 def get_project_data(red):
@@ -12,9 +12,12 @@ def get_project_data(red):
     # from redmine import Redmine
     # server = Redmine(red.redmine_url, key=red.key)
     # print server.projects[red.project]
-    url = "%s/projects/%s/issues.json" % (red.redmine_url, red.project)
+    url = "%s/projects/%s/issues.json?limit=%s&status_id=*" % (
+        red.redmine_url, red.project, red.limit)
     r = requests.get(url, auth=(red.user, red.pw))
     if r.status_code == 200:
+        log.info("success, url: %s" % url)
         return True, r.json()
     else:
+        log.info("error %s, url: %s" % (r.status_code, url))
         return False, "no response, code:  %s" % r.status_code
