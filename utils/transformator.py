322fa34efd167e94ce4379db69dca71a43de2fd0
rebel <rebel@retresco.de>
2013-11-13 01:12:21 +0100

    a lot of transformations

--- utils/transformator.py
+++ utils/transformator.py
@@ -1,14 +1,13 @@
 #!/usr/bin/env python
 # encoding: utf-8
 from utils.translator import t_
-# import logging as log
+import logging as log
 
 
 def restructure_data(data):
 
     # total_count: data['total_count']
     restructure = {}
-    # print "\n".join(data['issues'][0].keys())
     for issue in data['issues']:
         # Story, Task, Unterst...
         type_of = issue['tracker']['name']
@@ -48,45 +47,198 @@ def restructure_data(data):
         #
 
 
+
+
+
     new_items = {}
+    no_parent_task = False
+    no_parent_error = False
+    no_parent_feature = False
+
+
     for key, items in restructure.iteritems():
         # print key.encode('utf-8')
         # print items
         # print "\n".join(items[0].keys())
 
-        if key.encode('utf-8')  == "Story":
-            new_items[key] = []
+
+        if key.encode('utf-8') == "Story" or key.encode('utf-8') == "Arbeitspaket":
+            new_items[key] = new_items.get('Story', [])
+
             for item in items:
-                new_items[key].append(restructure_item(key, item))
+                newone = restructure_item(key, item)
+                if newone:
+                    new_items[key].append(newone)
 
-            # in order to sort the stories and put the backlogs at the end
-            # we prefix 'sprint ' to 'backlog' and remove it after the sorting
-            stories = new_items[key]
-            for story in stories:
-                if 'backlog' in story['fixed_version']:
-                    story['fixed_version'] = 'sprint ' + story['fixed_version']
-            stories = sorted(stories, key=lambda k: k['fixed_version'])
-            for story in stories:
-                if 'backlog' in story['fixed_version']:
-                    story['fixed_version'] = story['fixed_version'][7:]
+            # # in order to sort the stories and put the backlogs at the end
+            # # we prefix 'sprint ' to 'backlog' and remove it after the sorting
+            if 'fixed_version' in new_items[key][0]:
+                stories = new_items[key]
+                for story in stories:
+                    if 'fixed_version' not in story:
+                        story['fixed_version'] = 'None'
+                    if 'backlog' in story['fixed_version']:
+                        story['fixed_version'] = 'sprint ' + story['fixed_version']
+                stories = sorted(stories, key=lambda k: k['fixed_version'])
+                for story in stories:
+                    if 'backlog' in story['fixed_version']:
+                        story['fixed_version'] = story['fixed_version'][7:]
 
-            new_items[key] = stories
+                new_items[key] = stories
+
+
+
 
 
         elif key.encode('utf-8')  == "Task":
+
             alltasks = {}
+
             for item in items:
-                parentid = item['parent']['id']
-                tasks = alltasks.get(parentid, [])
-                tasks.append(restructure_item(key, item, parentid=parentid))
-                alltasks[parentid] = tasks
-            new_items[key] = alltasks
+
+                if 'parent' in item:
+                    parentid = item['parent']['id']
+                    tasks = alltasks.get(parentid, [])
+                    tasks.append(restructure_item("Task", item, parentid=parentid))
+                    alltasks[parentid] = tasks
+                else:
+                    no_parent_task = True
+                    tasks_without_parent = alltasks.get('no_parent_task', [])
+                    tasks_without_parent.append(restructure_item("Task", item))
+                    alltasks['no_parent_task'] = tasks_without_parent
+
+
+
+            thetasks = new_items.get("Task", {})
+            for k,v in alltasks.iteritems():
+                if k in thetasks:
+                    for x in v:
+                        thetasks[k].append(x)
+                else:
+                    thetasks[k] = v
+            new_items["Task"] = thetasks
+
+
 
         elif key.encode('utf-8')  == "Unterstützung":
-            # add support to stories
-            # new_items[key] = []
+
             for item in items:
+                if 'parent' in item:
+                    print 'support with parent'
+
                 new_items["Story"].append(restructure_item(key, item))
+
+
+
+        elif key.encode('utf-8')  == "Fehler" or key.encode('utf-8')  == "Feature":
+
+            x = key.encode('utf-8')
+            if x == "Fehler":
+                var = "error"
+            else:
+                var = "feature"
+            alltasks = {}
+            for item in items:
+
+                if 'parent' in item:
+                    print 'yes, parent in item. take care'
+                    # parentid = item['parent']['id']
+                    # tasks = alltasks.get(parentid, [])
+                    # tasks.append(restructure_item(key, item, parentid=parentid))
+                    # alltasks[parentid] = tasks
+                    pass
+                    # deal with them
+                else:
+                    if var == "error":
+                        no_parent_error = True
+                    else:
+                        no_parent_feature = True
+
+                    tasks_without_parent = alltasks.get('no_parent_%s' % var, [])
+                    tasks_without_parent.append(restructure_item(key, item))
+                    alltasks['no_parent_%s' % var] = tasks_without_parent
+
+            thetasks = new_items.get("Task", {})
+            for k,v in alltasks.iteritems():
+                if k in thetasks:
+                    for x in v:
+                        thetasks[k].append(x)
+                else:
+                    thetasks[k] = v
+            new_items["Task"] = thetasks
+
+
+
+
+
+
+        # done
+        # Story: 8 open / 9
+        # Task: 3 open / 10
+        # Unterstützung: 1 open / 1
+        # Fehler: 0 open / 2
+        # Feature << dummy story for feature ohne Story
+
+        # todo
+        # Arbeitspaket << ?! ?!
+
+        else:
+            print "another key ", key.encode('utf-8')
+            print items
+            print
+
+
+
+
+
+
+
+    # # at the end append a story dummy for every rubbish at least to show
+    # # may its just created on run
+    if no_parent_error:
+        no_parent_dummy = {
+            'id': 'no_parent_error',
+            'subject': 'All errors without a story',
+            'description': '',
+            'created_on': '',
+            'author': {'name': 'AutoGenerated'},
+            'assigned_to': {'name': ''},
+            'status': {'name': 'different'},
+        }
+        stories = new_items.get('Story', [])
+        the_dummy = restructure_item('Story', no_parent_dummy)
+        stories.append(the_dummy)
+        new_items['Story'] = stories
+
+    if no_parent_task:
+        no_parent_dummy = {
+            'id': 'no_parent_task',
+            'subject': 'All tasks without a story',
+            'description': '',
+            'created_on': '',
+            'author': {'name': 'AutoGenerated'},
+            'assigned_to': {'name': ''},
+            'status': {'name': 'different'},
+        }
+        stories = new_items.get('Story', [])
+        the_dummy = restructure_item('Story', no_parent_dummy)
+        stories.append(the_dummy)
+        new_items['Story'] = stories
+
+    if no_parent_feature:
+        no_parent_dummy = {
+            'id': 'no_parent_feature',
+            'subject': 'All features without a story',
+            'description': '',
+            'created_on': '',
+            'author': {'name': 'AutoGenerated'},
+            'assigned_to': {'name': ''},
+            'status': {'name': 'different'},
+        }
+        stories = new_items.get('Story', [])
+        the_dummy = restructure_item('Story', no_parent_dummy)
+        stories.append(the_dummy)
+        new_items['Story'] = stories
 
     return new_items
 
@@ -97,36 +249,54 @@ def restructure_data(data):
 
 def restructure_item(type_of, item, parentid=None):
 
-    new_item = {
-        'id': item['id'],
-        'subject': t_(item['subject']),
-        'description': t_(item['subject']),
-        'created_on': item['created_on'],
-        'author': t_(item['author']['name']),
-        'assigned_to': t_(item['assigned_to']['name']),
-        'fixed_version': t_(item['fixed_version']['name']).lower(),
-        'start_date': item['start_date'],
-        'status': t_(item['status']['name']),
-        # 'updated_on': item['updated_on'],
-        # 'done_ratio': item['done_ratio']
-        # 'due_date': item['due_date'],
-    }
+    try:
+        new_item = {
+            'id': item['id'],
+            'subject': t_(item['subject']),
+            'description': t_(item['subject']),
+            'created_on': item['created_on'],
+            'author': t_(item['author']['name']),
+            'assigned_to': t_(item['assigned_to']['name']),
+            'status': t_(item['status']['name'].lower().encode('utf-8')),
+            # 'updated_on': item['updated_on'],
+            # 'done_ratio': item['done_ratio']
+            # 'due_date': item['due_date'],
+        }
 
-    if type_of.encode('utf-8') == "Story":
-        # print item
-        new_item['type'] = 'story'
-        pass
+        try:
+            new_item['fixed_version'] = t_(item['fixed_version']['name']).lower()
+        except:
+            log.info("no such field")
 
-    elif type_of.encode('utf-8') == "Task":
-        if parentid:
-            new_item['parent'] = parentid
-        new_item['type'] = 'task'
+        for field in ['start_date']:
+            try:
+                new_item[field] = t_(item[field]).lower()
+            except:
+                log.info("no such field %s" % field)
 
-    elif type_of.encode('utf-8') == "Unterstützung":
-        # print item
-        new_item['type'] = 'support'
 
-        pass
 
-    return new_item
+
+
+
+        if type_of.encode('utf-8') == "Story":
+            # print item
+            new_item['type'] = 'story'
+            pass
+
+        elif type_of.encode('utf-8') == "Task":
+            if parentid:
+                new_item['parent'] = parentid
+            new_item['type'] = 'task'
+
+        elif type_of.encode('utf-8') == "Unterstützung":
+            # print item
+            new_item['type'] = 'support'
+
+            pass
+
+        return new_item
+    except Exception, e:
+        print e
+        return None
 
