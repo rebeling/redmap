976c8c44ea7fc9c8845a9c9d767d0516bd27fc99
rebel <rebel@retresco.de>
2013-11-14 00:26:57 +0100

    updated translations

--- redmine/__init__.py
+++ redmine/__init__.py
@@ -1,2 +1,22 @@
 #!/usr/bin/env python
 # encoding: utf-8
+
+# some stuff needs to be translated and this is specific to
+# redmine and the user settings
+# todo: find a nicer way to solve this problem more general
+
+translations = {
+    "backlog": "backlog",
+    "neu": "new",
+    "erledigt": "done",
+    "abgewiesen": "refused",
+    "in bearbeitung": "in-progress",
+    "gelöst": "qa !"
+}
+
+def t_(input, translations):
+    # translate or map input to
+    if input in translations:
+        return translations[input].encode('utf-8')
+    else:
+        return input.encode('utf-8')
