3f212188047213a3e7b1260c8b4d16432845cd2b
rebel <rebel@retresco.de>
2013-11-17 01:08:48 +0100

    finished tests of main workflow

--- start_test.py
+++ start_test.py
@@ -3,24 +3,38 @@
 import unittest
 from utils.config_parser import ConfigData
 from redmine.redreq import get_project_data
+from utils.transformator import restructure_data
+import json
 
 
-class ParseConfigTestCase(unittest.TestCase):
-
+class MainWorkflowTestCase(unittest.TestCase):
+    """
+        Base test class for ConfigFile processed properly,
+        connect to pm-tool and retrieve data, and jsonfy.
     """
 
-    Base test class for test the fields
-    of the ConfigFile processed properly.
+    def setUp(self):
+        self.cfg = ConfigData()
+        self.success, self.data_or_msg = get_project_data(self.cfg)
+        self.restructured = restructure_data(self.data_or_msg, self.cfg.project,
+                                        '%s' % self.cfg.url + self.cfg.project)
 
-    """
     def testConfRetrieval(self):
-        self.assertEqual(type(ConfigData().url), str)
-        self.assertEqual(type(ConfigData().key), str)
-        self.assertEqual(type(ConfigData().project), str)
+        self.assertEqual(type(self.cfg.url), str)
+        self.assertEqual(type(self.cfg.key), str)
+        self.assertEqual(type(self.cfg.project), str)
 
     def testProjectCall(self):
-        success, data_or_msg = get_project_data(ConfigData())
-        self.assertEqual(success, True)
+        self.assertEqual(self.success, True)
+
+    def testRestructuring(self):
+        self.assertEqual('info' in self.restructured, True)
+        self.assertEqual('task' in self.restructured, True)
+        self.assertEqual('story' in self.restructured, True)
+
+    def testTransformation(self):
+        final_data = json.dumps(self.restructured, indent=4, sort_keys=True)
+        self.assertEqual(type(final_data), str)
 
 
 if __name__ == '__main__':
