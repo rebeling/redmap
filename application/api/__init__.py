bae534fa48369ab0681ae5c45ce89b32a16f8c24
rebel <rebel@retresco.de>
2013-11-20 01:05:58 +0100

    updated the route for better access

--- application/api/__init__.py
+++ application/api/__init__.py
@@ -10,9 +10,9 @@ def deliver_content():
     with open('application/data/content.json', 'r') as f:
         return f.read()
 
-@app.route("/update/order/<type_of>/<id>/<direction>")
-def update_order(type_of, id, direction):
-    result = update(type_of, id, direction)
+@app.route("/update/order/<type_of>/<storyid>/<taskid>/<direction>")
+def update_order(type_of, storyid, taskid, direction):
+    result = update(type_of, storyid, taskid, direction)
     return result
 
 
