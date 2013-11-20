44b1102f6caf58a5af863d8da18098346ae31a9c
rebel <rebel@retresco.de>
2013-11-20 22:10:15 +0100

    modified to be general

--- application/utils/__init__.py
+++ application/utils/__init__.py
@@ -10,13 +10,16 @@ def get_week_num_of(thisdate=None):
     print Year,WeekNum,DOW
     return WeekNum
 
+
 def write_content_to(filepath, content):
     with open(filepath, 'w') as f:
         f.write(content)
 
-def order_by_position(list_of_dicts):
-    newlist = sorted(list_of_dicts, key=lambda k: k['position'])
+
+def order_by_key(list_of_dicts, key2order):
+    newlist = sorted(list_of_dicts, key=lambda k: k[key2order])
     return newlist
+
 
 if __name__ == '__main__':
     get_week_num_of()
