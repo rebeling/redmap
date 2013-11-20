69cadffa9d01ddec6774bd3448492b5020a99574
rebel <rebel@retresco.de>
2013-11-20 22:09:39 +0100

    start via args

---
+++ application/redmine/translation.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+# some stuff needs to be translated/transformed and this is specific to
+# redmine and the user settings: languages, type of elements etc.
+# todo: find a nicer way to solve this problem more general
+translations = {
+    "backlog": "backlog",
+    "neu": "new",
+    "erledigt": "done",
+    "abgewiesen": "refused",
+    "in bearbeitung": "in-progress",
+    "gelöst": "qa !",
+    "arbeitspaket": "package",
+    "fehler": "bug",
+    "unterstützung": "support"
+}
+
+def red_t_(input):
+    if input in translations:
+        return translations[input]
+    else:
+        return input
+
--- start.py
+++ start.py
@@ -1,45 +1,25 @@
 #!/usr/bin/env python
 # encoding: utf-8
 from application.utils.config_parser import ConfigData
-from application.redmine.redreq import get_project_data
-from application.utils.transformator import restructure_data
-from application.utils import write_content_to
-import logging as log
 import logging.config
-import json
+import logging as log
+import sys
 
 
 def main():
-    """
-        1. get/set all your connection data for redmine
-        2. connect to your project and get the projects issues.json
-        3. restructure the data in transformator
-        4. save data as json
-            todo:
-            store data somehow ...pouchdb or redis would be awesome!
-    """
     logging.config.fileConfig('cfg/logging.cfg')
-    log.info('Started')
-
     red = ConfigData()
-    success, data_or_msg = get_project_data(red)
+    cl_args = sys.argv
+    log.info("args: %s" % cl_args)
 
-    if success:
-        restructured = restructure_data(data_or_msg,
-                                        red.project,
-                                        '%s' % red.url + red.project)
-        final_data = json.dumps(restructured, indent=4, sort_keys=True)
-        write_content_to('application/data/content.json', final_data)
+    if 'red' in cl_args:
+        from application.redmine import process_project
+        process_project(red)
 
-        log.info('json file craeted')
-    else:
-        log.info(data_or_msg)
-
-    log.info('Finished')
+    if 'api' in cl_args:
+        from application.api import app
+        app.run(debug=True)
 
 
 if __name__ == '__main__':
-    # main()
-    from application.api import app
-    app.run(debug=True)
-
+    main()
