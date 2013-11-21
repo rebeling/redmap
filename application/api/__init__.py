e85b62cd84908f3024c24225f1f855b3c9c7fc03
rebel <rebel@retresco.de>
2013-11-21 23:50:35 +0100

    variable wording

--- application/api/__init__.py
+++ application/api/__init__.py
@@ -10,10 +10,11 @@ def deliver_content():
     with open('application/data/content.json', 'r') as f:
         return f.read()
 
+
 @app.route("/update/order/<type_of>/<storyid>/<taskid>/<direction>")
 def update_order(type_of, storyid, taskid, direction):
-    the_updated = update(type_of, storyid, taskid, direction)
-    return the_updated
+    updated_json = update(type_of, storyid, taskid, direction)
+    return updated_json
 
 
 if __name__ == "__main__":
