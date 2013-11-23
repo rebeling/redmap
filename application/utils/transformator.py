bdb9c7680901fdc9886552606a1f47a001a1914f
rebel <rebel@retresco.de>
2013-11-24 00:47:00 +0100

    refactored the whole processing

--- application/utils/transformator.py
+++ application/utils/transformator.py
@@ -3,8 +3,11 @@
 from application.redmine.translation import red_t_
 from application.redmine.redreq import get_issue_details
 from application.utils import order_by_key
+from application.utils.story_and_tasks import create_item # Story, Task
 import logging as log
 import json
+from application.utils import timing
+import time
 
 
 def organize_issues_by_type(data):
@@ -19,40 +22,20 @@ def organize_issues_by_type(data):
     return restructure
 
 
-def process(item, alltasks, key, no_parent):
-    if 'parent' in item:
-        parentid = item['parent']['id']
-        tasks = alltasks.get(parentid, [])
-        tasks.append(restructure_item("task", item, parentid=parentid))
-        alltasks[parentid] = tasks
-    else:
-        no_parent[key] = True
-        tasks_without_parent = alltasks.get('no_parent_%s' % key, [])
-        tasks_without_parent.append(restructure_item("task", item))
-        alltasks['no_parent_%s' % key] = tasks_without_parent
-    return alltasks, no_parent
-
-
-def extend_thetasks(alltasks, processed_items):
-    thetasks = processed_items.get("task", {})
-    for k,v in alltasks.iteritems():
-        if k in thetasks:
-            for x in v:
-                thetasks[k].append(x)
-        else:
-            thetasks[k] = v
-    return thetasks
-
-
+@timing
 def restructure_data(data, project, project_url):
     """
-        input is a json structure of the complete project
-        so is the output, but restructured
-        1. organize issues by type
-        2. process for every type the issues list
-        3. ..
+    input is a json structure of the complete project
+    so is the output, but restructured
+    1. organize issues by type
+    2. process for every type the issues list
+    3. ..
 
     """
+    t1 = time.time()
+
+    print "» %.2f sec" % (time.time() - t1)
+
     processed_items = {
         'story': [],
         'task': {},
@@ -65,63 +48,109 @@ def restructure_data(data, project, project_url):
 
     restructure = organize_issues_by_type(data)
 
+    # get data from last time to update position and status
+    updater = {}
+    try:
+        with open('application/data/content.json', 'r') as f:
+            buffer = json.loads(f.read())
+            updater['story'] = {
+                x['id']:{'postion': x['position'], 'status': x['status']}
+                for x in buffer['story']}
+
+            updater['task'] = {}
+            for key, tasks in buffer['task'].iteritems():
+                updater['task'][key] = {
+                    x['id']:{'postion': x['position'], 'status': x['status']}
+                    for x in tasks}
+
+    except Exception, e:
+        log.debug("failed to update with latest data: %s" % e)
+
+
+    print "» %.2f sec" % (time.time() - t1)
+
     # create no_parent for all the orphans
     no_parent = {x: False
         for x in ['task', 'package', 'support', 'bug', 'feature']}
 
+    print "» %.2f sec" % (time.time() - t1)
+
     for key, items in restructure.iteritems():
 
         if key == "story":
+
+            print "story » %.2f sec" % (time.time() - t1)
+
             for i, item in enumerate(items):
-                newone = restructure_item(key, item, position=i)
-                if newone:
-                    processed_items[key].append(newone)
+
+                success, details = get_issue_details(item['id'])
+                details['type'] = 'story'
+                details['position'] = i + 1
+                item.update(details)
+
+                s = create_item(item)
+                # s = Story(item)
+
+                if updater and 'story' in updater:
+                    # update with data from last time
+                    this = updater['story'].get(item['id'], None)
+                    if this:
+                        s['position'] = this['position']
+                        s['status'] = this['status']
+
+                processed_items["story"].append(s)
 
         elif key == "support" or key == "task":
 
-            buffer = {}
-            for item in items:
-                buffer, no_parent = process(item, buffer, key, no_parent)
-            processed_items["task"] = extend_thetasks(buffer, processed_items)
+            print "task » %.2f sec" % (time.time() - t1)
 
-        elif key == "bug" or key == "feature":
-
-            buffer = {}
             for item in items:
 
-                if 'parent' in item:
-                    print 'yes, parent in item. take care'
-                    pass
-                else:
-                    no_parent[key] = True
-                    buffer, no_parent = process(item, buffer, key, no_parent)
+                success, details = get_issue_details(item['id'])
+                details['type'] = 'task'
+                item.update(details)
 
-            processed_items["task"] = extend_thetasks(buffer, processed_items)
+                t = create_item(item)
+
+                parent_id = t['parent']
+                if parent_id == None:
+                    parent_id = 'no_parent_%s' % key
+
+                if updater and 'task' in updater:
+                    if parent_id in updater['task']:
+                        this = updater['task'][parent_id].get(item['id'], None)
+                        if this:
+                            t['position'] = this['position']
+                            t['status'] = this['status']
+                    else:
+                        # initially set based on len of processed tasks
+                        p = len(processed_items["task"].get(parent_id, [])) + 1
+                        t['position'] = p
+
+                tasks_4_storyX = processed_items["task"].get(parent_id, [])
+                tasks_4_storyX.append(t)
+                processed_items["task"][parent_id] = tasks_4_storyX
 
         else:
+
+            print "» %.2f sec" % (time.time() - t1)
+
             log.info("another key: %s" % key)
 
+    print "» %.2f sec" % (time.time() - t1)
 
     processed_items = story_dummies_for_orphans(processed_items, no_parent)
 
-    for key in processed_items['story']:
-        # process the tasks again and numerate them
-        try:
-            for i,task in enumerate(processed_items['task'][key['id']]):
-                task['position'] = i
-        except:
-            pass
-
-    processed_items = check_latest(processed_items)
     processed_items = items_order(processed_items)
 
     return processed_items
 
 
+@timing
 def story_dummies_for_orphans(items, no_parent):
     """
-        at the end append a story dummy for every rubbish at least to show
-        identify false positives in the project
+    at the end append a story dummy for every rubbish at least to show
+    identify false positives in the project
 
     """
     dummies = {
@@ -135,64 +164,33 @@ def story_dummies_for_orphans(items, no_parent):
                     'subject': 'All tasks without a story'}
     }
 
-    dummy_pos = len(items['story'])
+    i = len(items['story'])
     for key, value in no_parent.iteritems():
+
         if value:
-            dummy_pos += 1
-            buffer = add_a_dummy_story(dummies, key, position=dummy_pos)
-            items['story'].append(buffer)
+            i += 1
+            no_parent_item = {
+                'id': dummies[key]['id'],
+                'subject': dummies[key]['subject'],
+                'status': {'name': 'unknown'},
+                'position': i
+            }
+            s = Story(no_parent_item)
+            items['story'].append(s.item)
+
     return items
 
 
-def check_latest(processed_items):
-    """
-        update the new with the content from latest
-
-    """
-    try:
-        with open('application/data/content.json', 'r') as f:
-            old_processed_items = json.loads(f.read())
-            for item in old_processed_items['story']:
-                for new_item in processed_items['story']:
-                    if item['id'] == new_item['id']:
-                        new_item['position'] = item['position']
-                        new_item['sprint'] = item['sprint']
-    except Exception, e:
-        log.debug("failed to update with latest data: %s" % e)
-
-    try:
-        with open('application/data/content.json', 'r') as f:
-            old_processed_items = json.loads(f.read())
-            for key, value in old_processed_items['task'].iteritems():
-                for old_item in value:
-                    try:
-                        key = int(key)
-                    except:
-                        pass
-                    if key in processed_items['task']:
-                        for new_item in processed_items['task'][key]:
-                            if old_item['id'] == new_item['id']:
-                                new_item['position'] = old_item['position']
-                                new_item['sprint'] = old_item['sprint']
-                    else:
-                        log.debug("key not in there %s %s " % (key, type(key)) )
-                        for x in processed_items['task'].keys():
-                            log.debug("key %s %s" % (x, type(x)))
-    except Exception, e:
-        log.debug("failed to update with latest data: %s" % e)
-
-    return processed_items
-
-
+@timing
 def items_order(items):
     """
-        order the items by position. easy for stories, but loopy for tasks
+    order the items by position. easy for stories, but loopy for tasks
 
     """
     items['story'] = order_by_key(items['story'], 'position')
 
+    # tasks
     for key, to_be_ordered in items['task'].iteritems():
-        log.debug("order for %s" % key)
         try:
             items['task'][key] = order_by_key(to_be_ordered, 'position')
         except Exception, e:
@@ -202,61 +200,16 @@ def items_order(items):
 
 
 
-def add_a_dummy_story(dummies, key, position=0):
-    no_parent_dummy = {
-        'id': dummies[key]['id'],
-        'subject': dummies[key]['subject'],
-        'description': '',
-        'created_on': '',
-        'author': {'name': 'AutoGenerated'},
-        'assigned_to': {'name': ''},
-        'status': {'name': 'unknown'},
-    }
-    return restructure_item('story', no_parent_dummy, position=position, dummy=1)
 
 
-def restructure_item(type_of, item, parentid=None, position=0, dummy=None):
-
-    # get more details
-    done_ratio = 0
-    spent_hours = 0
-    if dummy == None:
-        success, data = get_issue_details(item['id'])
-        if success:
-            done_ratio = data['issue'].get('done_ratio', 0)
-            spent_hours = data['issue'].get('spent_hours', 0)
-        else:
-            log.debug("get issue error: %s" % data)
 
 
-    # init the new item
-    new_item = {
-        'id': item['id'],
-        'subject': red_t_(item['subject'].encode('utf-8')),
-        'description': red_t_(item['description'].encode('utf-8')),
-        'created_on': item['created_on'],
-        'author': red_t_(item['author']['name'].encode('utf-8')),
-        'status': red_t_(item['status']['name'].lower().encode('utf-8')),
-        'type': type_of,
-        'position': position,
-        'sprint': 'initial',
-        'done_ratio': done_ratio,
-        'spent_hours': spent_hours
-    }
 
-    for x in ['fixed_version', 'assigned_to']:
-        try:
-            new_item[x] = red_t_(item[x]['name'].lower().encode('utf-8'))
-        except:
-            log.info("no such field %s" % x)
 
-    for field in ['start_date', 'estimated_hours']:
-        try:
-            new_item[field] = item[field]
-        except:
-            log.info("no such field %s" % field)
 
-    return new_item
+
+
+
 
 
 
